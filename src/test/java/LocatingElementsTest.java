19af1be18337ce70ee9c367d8955742aa2c49e07
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 06:27:58 -0300

    Test: create test typesOfLocatorsTest

--- src/test/java/LocatingElementsTest.java
+++ src/test/java/LocatingElementsTest.java
@@ -25,4 +25,27 @@ public class LocatingElementsTest {
         driver.quit();
     }
 
+    @Test
+    public void typesOfLocatorsTest() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement swagLabsLogo = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div[1]")));
+        WebElement userNameInput = driver.findElement(By.id("user-name"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        WebElement loginButton = driver.findElement(By.cssSelector("#login-button"));
+        WebElement botImage = driver.findElement(By.className("bot_column"));
+
+        Assert.assertTrue(swagLabsLogo.isDisplayed());
+        Assert.assertTrue(userNameInput.isDisplayed());
+        Assert.assertTrue(passwordInput.isDisplayed());
+        Assert.assertTrue(loginButton.isDisplayed());
+        Assert.assertTrue(botImage.isDisplayed());
+
+        driver.quit();
+    }
+
 }
