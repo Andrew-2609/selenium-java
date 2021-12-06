908e0aac76da72e2e01ce40b39068b209ed618a2
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-05 22:48:52 -0300

    Test: create testOnFirefox test

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SeleniumTest {
+
+    @Test
+    public void testOnFirefox() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.quit();
+    }
 
 }
