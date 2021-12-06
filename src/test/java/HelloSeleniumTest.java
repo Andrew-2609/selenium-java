4dc5fa572f032039520cd9190004403ff4824637
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-05 22:21:06 -0300

    Feat: create simple first test using WebDriver and Chrome WebDriverManager

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,2 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HelloSeleniumTest {
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void firstTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.quit();
+    }
+
 }
