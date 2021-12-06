4624c22dabb5dd78ed7935110626ade21964786b
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 06:01:51 -0300

    Test: create test firstElementsQuiz

--- src/test/java/LocatingElementsTest.java
+++ src/test/java/LocatingElementsTest.java
@@ -1,2 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LocatingElementsTest {
+
+    @Test
+    public void firstElementsQuiz() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("user-name")));
+
+        Assert.assertTrue(element.isDisplayed());
+
+        driver.quit();
+    }
+
 }
